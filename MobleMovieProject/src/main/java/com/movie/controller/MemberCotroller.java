56c16c05ef17838b3d5335dfcd179027761f7be2
jaebeoms <woqja0192@gmail.com>
2022-10-27 17:16:30 +0900

    jwt Security 토큰 생성

--- MobleMovieProject/src/main/java/com/movie/controller/MemberCotroller.java
+++ MobleMovieProject/src/main/java/com/movie/controller/MemberCotroller.java
@@ -1,5 +1,7 @@
 package com.movie.controller;
 
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -27,6 +29,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.movie.service.MemberService;
 import com.movie.vo.MemberVo;
+import com.movie.vo.TokenVo;
 
 @RestController
 @CrossOrigin
@@ -44,16 +47,11 @@ public class MemberCotroller {
 	
 	//로그인
 	@PostMapping("/login")
-	public String login(MemberVo memberVo,@RequestParam("member_account")String member_account,@RequestParam("member_pw")String member_pw) {
-		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-		
-		
-		if(passwordEncoder.matches(member_pw, memberVo.getMember_pw())) {
-			 return memberService.loginToken(member_account);
-		}else {
-			return "일치하지 않습니다.";
-		}
-
+	public String login(@RequestBody Map map) {
+		TokenVo token = new TokenVo();
+		token.setMember_account(map.get("member_account").toString());
+		token.setMember_pw(map.get("member_pw").toString());
+		return memberService.loginToken(token);
 	}
 //	//로그인 실패
 //	@GetMapping("/login_fail")
@@ -126,27 +124,33 @@ public class MemberCotroller {
 				sb.setCharAt(i, '*');
 			}
 			id = sb.toString();
-			return "아이디 : " + id;
+			return id;
 		}
 	}
 	
 	//인증번호 생성 및 비밀번호 재설정
-	@GetMapping("/find_pw")
-	public String findPw(MemberVo memberVo,@RequestParam("member_account")String member_account, 
-			@RequestParam("member_name")String member_name,@RequestParam("member_email")String member_email,
-			@RequestParam("member_pw")String member_pw, @RequestParam("member_code")int member_code) {
-		int resultCode = memberService.CreateCode(member_account, member_name, member_email);
-		if(resultCode == 1) {
-			int resultPw = memberService.resetPW(member_pw, member_email, member_code);
-			if(resultPw == 1) {
-				return "비밀번호가 재설정 되었습니다.";
-			}else {
-				return "인증번호가 다릅니다.";
-			}
-		}else {
-			return "일치하는 정보가 없습니다.";
-		}
-	}
-	
-
+	 @GetMapping("/find_pw")
+     public String findPw(@RequestParam("member_account")String member_account, @RequestParam("member_name")String member_name,
+           @RequestParam("member_email")String member_email) {
+        System.out.println(member_account + member_name + member_email);
+        int result = memberService.CreateCode(member_account, member_name, member_email);
+        if(result == 1) {
+           return "인증번호가 전송되었습니다.";
+        }
+        else {
+           return "일치하는 정보가 없습니다.";
+        }
+     }
+     
+     //비밀번호 재설정
+     @GetMapping("/reset_pw")
+     public String resetPw(@RequestParam("member_email")String member_email,@RequestParam("member_code")int member_code,
+           @RequestParam("member_pw")String member_pw) {
+        int result = memberService.resetPW(member_pw, member_email, member_code);
+        if(result == 1) {
+           return "비밀번호가 재설정 되었습니다.";
+        }else {
+           return "인증번호가 다릅니다.";
+        }
+     }
 }
